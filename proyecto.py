cf61d67dbbcf86d420b6921e6eb8f3852266e5d4
ariadnamedina <122551098+ariadnamedina@users.noreply.github.com>
2025-08-05 17:46:23 -0400

    Copiando el apendice A del enunciado.py

--- proyecto.py
+++ proyecto.py
@@ -1,3 +1,5 @@
+import requests
+from PIL import Image
 class Departamento:
     def __init__(self, id, nombre):
         self.id = id
@@ -7,6 +9,42 @@ class Departamento:
         return f"Id: {self.id} - nombre departamento: {self.nombre}"
     
     
+def guardar_imagen_desde_url(url, nombre_archivo):
+    """
+    Descarga una imagen desde una URL y la guarda en un archivo.
+    """
+    try:
+        response = requests.get(url, stream=True)
+        response.raise_for_status()  # Lanza una excepción para códigos de estado de error (4xx o 5xx)
+        
+        content_type = response.headers.get('Content-Type')
+        extension = '.png'  # Valor por defecto
+        if content_type:
+            if 'image/png' in content_type:
+                extension = '.png'
+            elif 'image/jpeg' in content_type:
+                extension = '.jpg'
+            elif 'image/svg+xml' in content_type:
+                extension = '.svg'
+        
+        nombre_archivo_final = f"{nombre_archivo}{extension}"
+        with open(nombre_archivo_final, 'wb') as file:
+            for chunk in response.iter_content(chunk_size=8192):
+                file.write(chunk)
+        
+        print(f"Imagen guardada exitosamente como '{nombre_archivo_final}'")
+        return nombre_archivo_final
+    
+    except requests.exceptions.RequestException as e:
+        print(f"Error al hacer el request: {e}")
+    except IOError as e:
+        print(f"Error al escribir el archivo: {e}")
+    return None
+
+def abrir_imagen(archivo):
+
+    img = Image.open(archivo)
+    img.show()
 
 def mostrar_menu():
     print("--- MetroArt ---")
